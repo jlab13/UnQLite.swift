5430b31ebbca48959a215ddf0c2cde689864bebf
JLab13 <jlab13@gmail.com>
2018-08-31 00:59:19 +0300

    KeyValue TestCase

--- Tests/UnQLiteTests/UnQLiteTests.swift
+++ Tests/UnQLiteTests/UnQLiteTests.swift
@@ -5,21 +5,70 @@ import XCTest
 final class UnQLiteTests: XCTestCase {
 
     static var allTests = [
-        ("testKeyValue", testKeyValue),
+        ("KeyValueInt", testKeyValueInt),
+        ("KeyValueFloat", testKeyValueFloat),
+        ("KeyValueDouble", testKeyValueDouble),
     ]
     
 
-    func testKeyValue() throws {
+    func testKeyValueInt() throws {
     	let db = try UnQLite(fileName: ":mem:", mode: .inMemory)
 
-    	try db.set(0, forKey: "int")
-    	XCTAssertEqual(try db.integer(forKey: "int"), 0)
+    	try db.set(0, forKey: "int_zero")
+    	XCTAssertEqual(try db.integer(forKey: "int_zero"), 0)
 
-    	try db.set(Int.max, forKey: "int")
-    	XCTAssertEqual(try db.integer(forKey: "int"), Int.max)
+    	try db.set(Int.max, forKey: "int_max")
+    	XCTAssertEqual(try db.integer(forKey: "int_max"), Int.max)
 
-    	try db.set(Int.min, forKey: "int")
-    	XCTAssertEqual(try db.integer(forKey: "int"), Int.min)
+    	try db.set(Int.min, forKey: "int_min")
+    	XCTAssertEqual(try db.integer(forKey: "int_min"), Int.min)
+        
+        let initialInt = 13
+        try db.setNumeric(initialInt, forKey: "int_generic")
+        let readedInt: Int = try db.numeric(forKey: "int_generic")
+        XCTAssertEqual(initialInt, readedInt)
+    }
+    
+    func testKeyValueFloat() throws {
+        let db = try UnQLite(fileName: ":mem:", mode: .inMemory)
+
+        try db.set(Float(0), forKey: "float_zero")
+        XCTAssertEqual(try db.float(forKey: "float_zero"), 0)
+
+        try db.set(Float.greatestFiniteMagnitude, forKey: "float_01")
+        XCTAssertEqual(try db.float(forKey: "float_01"), Float.greatestFiniteMagnitude)
+
+        try db.set(Float.leastNormalMagnitude, forKey: "float_02")
+        XCTAssertEqual(try db.float(forKey: "float_02"), Float.leastNormalMagnitude)
+
+        try db.set(Float.leastNonzeroMagnitude, forKey: "float_03")
+        XCTAssertEqual(try db.float(forKey: "float_03"), Float.leastNonzeroMagnitude)
+
+        let initialFloat: Float = 13.666
+        try db.setNumeric(initialFloat, forKey: "float_generic")
+        let readedFloat: Float = try db.numeric(forKey: "float_generic")
+        XCTAssertEqual(initialFloat, readedFloat)
+    }
+
+    func testKeyValueDouble() throws {
+        let db = try UnQLite(fileName: ":mem:", mode: .inMemory)
+        
+        try db.set(Double(0), forKey: "double_zero")
+        XCTAssertEqual(try db.double(forKey: "double_zero"), 0)
+        
+        try db.set(Double.greatestFiniteMagnitude, forKey: "double_01")
+        XCTAssertEqual(try db.double(forKey: "double_01"), Double.greatestFiniteMagnitude)
+        
+        try db.set(Double.leastNormalMagnitude, forKey: "double_02")
+        XCTAssertEqual(try db.double(forKey: "double_02"), Double.leastNormalMagnitude)
+        
+        try db.set(Double.leastNonzeroMagnitude, forKey: "double_03")
+        XCTAssertEqual(try db.double(forKey: "double_03"), Double.leastNonzeroMagnitude)
+        
+        let initialDouble: Double = 13.666
+        try db.setNumeric(initialDouble, forKey: "double_generic")
+        let readedDouble: Double = try db.numeric(forKey: "double_generic")
+        XCTAssertEqual(initialDouble, readedDouble)
     }
 
 }
